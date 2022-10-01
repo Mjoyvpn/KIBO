7c3d109971e75d066b8c8d8082ee270ebf098595
Mjoyvpn <112147124+Mjoyvpn@users.noreply.github.com>
2022-10-01 23:25:41 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -104,13 +104,13 @@ echo -e "IP-VPS          :  $IPVPS"
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;41;36m                 PANEL MENU                 \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
-echo -e " [\033[1;36m01\033[0m] Add User SSH   [\033[1;36m07\033[0m]  Add Vmess Account
+echo -e " [\033[1;36m01\033[0m] Add User SSH   [\033[1;36m07\033[0m] Add Vmess Account
  [\033[1;36m02\033[0m] SSH WS Enable  [\033[1;36m08\033[0m] Add Vless Account
  [\033[1;36m03\033[0m] Cek User SSH   [\033[1;36m09\033[0m] Add Trojan Account
  [\033[1;36m04\033[0m] Del User SSH   [\033[1;36m10\033[0m] Add Sdosok Account
  [\033[1;36m05\033[0m] Renew SSH      [\033[1;36m11\033[0m] Cek User Xray
  [\033[1;36m06\033[0m] Member         [\033[1;36m12\033[0m] Del User Xray
- ${GREEN}Script ARTA MAULANA\033[0m [\033[1;36m13\033[0m] Renew User Xray"
+ ${GREEN}Script by joysmark\033[0m [\033[1;36m13\033[0m] Renew User Xray"
 echo -e  "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;41;36m               Settings MENU                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
@@ -133,18 +133,12 @@ echo ""
 #echo -e "\e[36m╘════════════════════════════════════════════════════╛\033[0m"
 echo -e "\e[36m╒═════════════════════════════════════════════╕\033[0m"
 if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
-echo -e "Version       :\033[1;36m $(cat /opt/.ver) Latest Version\e[0m"
 echo -e "Client Name   : $Name"
-echo -e "Expiry script : $Exp"
-rm -f /home/needupdate > /dev/null 2>&1
+echo -e "Expiry script : LIFETIME"
 else
-rm /dev/.permiss > /dev/null 2>&1
-touch /home/needupdate > /dev/null 2>&1
-echo -e "\033[0;33mVersion : $(cat /opt/.ver) Update available to $serverV\e[0m"
 echo -e "\e[36m╒═════════════════════════════════════════════╕\033[0m"
 echo ""
 echo -e "[ \033[0;31mChangelog\033[0m ]"
-curl -sS https://raw.githubusercontent.com/Mjoyvpn/KIBO/main/clgshow
 echo -e "
 "
 fi
@@ -153,58 +147,6 @@ echo
 echo -ne "Select menu : "; read x
 if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
     if [[ $x -eq 1 ]]; then
-       usernew
-    elif [[ $x -eq 2 ]]; then
-       sshws
-    elif [[ $x -eq 3 ]]; then
-       cek
-    elif [[ $x -eq 4 ]]; then
-       hapus
-    elif [[ $x -eq 5 ]]; then
-       renew
-    elif [[ $x -eq 6 ]]; then
-       member
-    elif [[ $x -eq 7 ]]; then
-       add-ws
-    elif [[ $x -eq 8 ]]; then
-       add-vless
-    elif [[ $x -eq 9 ]]; then
-       add-tr
-    elif [[ $x -eq 10 ]]; then
-       add-ssws
-    elif [[ $x -eq 11 ]]; then
-       cek-user
-    elif [[ $x -eq 12 ]]; then
-       del-user
-    elif [[ $x -eq 13 ]]; then
-       renew-xray
-    elif [[ $x -eq 14 ]]; then
-       setting-menu
-    elif [[ $x -eq 15 ]]; then
-       crtv2ray
-    elif [[ $x -eq 16 ]]; then
-       running
-    elif [[ $x -eq 17 ]]; then
-       reboot
-    elif [[ $x -eq 18 ]]; then
-       restart
-    elif [[ $x -eq 19 ]]; then
-       passwd
-    elif [[ $x -eq 20 ]]; then
-       autoreboot
-    elif [[ $x -eq 21 ]]; then
-       vnstat
-    elif [[ $x -eq 22 ]]; then
-       vnstat -d
-    else
-       menu
-    fi
-else
-    if [[ $x -eq 69 ]]; then
-       wget -q -O /usr/bin/update-script "https://raw.githubusercontent.com/Mjoyvpn/KIBO/main/setup.sh" && chmod +x /usr/bin/update-script
-       screen -S upds update-script
-       menu
-    elif [[ $x -eq 1 ]]; then
        usernew
     elif [[ $x -eq 2 ]]; then
        sshws
